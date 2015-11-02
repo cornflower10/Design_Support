c966e479df025263a17cfae9951537154a8dcb8a
cornflower10 <861281331@qq.com>
2015-11-02 15:31:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiejingbao/design_support/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.xiejingbao.design_support;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+
+public class MainActivity extends AppCompatActivity {
+private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("fhksahf");
+        //根据api大小设置状态栏的颜色
+        StatusBarCompat.compat(this,getResources().getColor(R.color.status_bar_color));
+        setSupportActionBar(toolbar);
+//        toolbar.setBackgroundColor(Color.BLUE);
+
+    }
+
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
